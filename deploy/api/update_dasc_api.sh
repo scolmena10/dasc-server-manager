583cc1cd754b30221fe72d51f8b7e671bb8fe91e
scolmena10 <scolmena@institutmvm.cat>
2026-04-13 13:56:33 +0200

    Update update_dasc_api.sh

--- deploy/api/update_dasc_api.sh
+++ deploy/api/update_dasc_api.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-REPO_DIR="$HOME/dasc-server-manager"
+REPO_DIR="/home/${SUDO_USER}/dasc-server-manager"
 INSTALL_DIR="/opt/dasc/api"
 SERVICE_NAME="dasc-api"
 
@@ -10,12 +10,12 @@ cd "$REPO_DIR"
 git pull
 
 echo "==> Actualizando archivos del panel"
-cp -r "$REPO_DIR/deploy/api/package/"* "$INSTALL_DIR/"
+rsync -av --exclude=config.env deploy/api/package/ "$INSTALL_DIR/"
 
-echo "==> Instalando nuevas dependencias (si hay)"
+echo "==> Instalando dependencias (si hay cambios)"
 "$INSTALL_DIR/venv/bin/pip" install -r "$INSTALL_DIR/requirements.txt"
 
 echo "==> Reiniciando servicio"
-sudo systemctl restart "$SERVICE_NAME"
+systemctl restart "$SERVICE_NAME"
 
 echo "==> Update completado"
